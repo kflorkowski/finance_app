1fe71caaa5c7a82d1274d857619dfa48ea457ecc
kflorkowski <k.florkowski98@gmail.com>
2024-09-04 16:58:04 +0200

    bug fix

---
+++ user/tests/test_views.py
@@ -0,0 +1,14 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib import messages
+# from 
+
+def test_register():
+    pass
+
+def test_login():
+    pass
+
+def test_logout():
+    pass
--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('welcome', views.welcome, name='welcome'),
+    path('welcome/', views.welcome, name='welcome'),
 ]
