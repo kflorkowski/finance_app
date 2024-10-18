8989ce061920004f32f94aeec00633813407973e
kflorkowski <k.florkowski98@gmail.com>
2024-10-18 15:47:10 +0200

    css & others

--- finance_app/urls.py
+++ finance_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('user.urls')),
-    path('', include('my_app.urls'))
+    path('', include('my_app.urls')),
 ]
